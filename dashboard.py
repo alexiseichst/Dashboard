165abf644d38e8e95afb158b7f6f5f94b789b5fd
alexiseichst <alexiseichst@gmail.com>
2020-03-02 19:06:16 +0100

    Code review

--- dashboard.py
+++ dashboard.py
@@ -12,126 +12,123 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-class command:
-    executable = ''
-    args = []
-    std = ''
-
+class Command:
     # Constructor with executable name and std type
     def __init__(self, executable, std):  
-        self.executable = executable
-        self.std = std
-        self.args = []
+        self.__executable = executable
+        self.__std = std
+        self.__args = []
 
     def __str__(self):
         rt = ''
-        rt+='Executable: {}'.format(self.executable)
-        for arg in self.args:
-            rt+='\n\t\tArg: {}'.format(arg)
+        rt+='Executable: {}'.format(self.__executable)
+        for arg in self.__args:
+            rt+='\n  Arg: {}'.format(arg)
         return rt
 
     # Add argumment of the command
-    def addArg(self,arg):  
-        self.args.append(arg)
+    def add_arg(self,arg):  
+        self.__args.append(arg)
 
     # Execute the command
     def exec(self):
         rt = {}
-        command = [self.executable]
-        command += self.args
+        command = [self.__executable]
+        command += self.__args
         process = subprocess.Popen(command,
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE)
         rt['stdout'], rt['stderr'] = process.communicate()
         return rt
 
-class instruction:
-    name = ''
-    description = ''
-    commands = []
+class Instruction:
 
     # constructor
     def __init__(self, name):  
-        self.name = name
-        self.description = ''
-        self.commands = []
+        self.__name = name
+        self.__description = ''
+        self.__commands = []
     
     def __str__(self):
         rt = ''
         rt+='{}'.format(self.name)
-        rt+='\n\tDescription: {}'.format(self.description)
-        rt+='\n\tResult: {}'.format(self.getResult())
-        for c in self.commands:
-            rt+='\n\t{}'.format(c.__str__())
+        rt+='\n Description: {}'.format(self.__description)
+        rt+='\n Result: {}'.format(self.result)
+        for c in self.__commands:
+            rt+='\n {}'.format(c.__str__())
         return rt
 
-    # Description   
-    def setName(self,name):
-        self.name = name
+    @property
+    def name(self):
+        return self.__name
 
-    def getName(self):
-        return self.name
+    @name.setter
+    def name(self, value):
+        self.__name = value
 
-    # Description   
-    def setDescription(self,description):  
-        self.description = description
+    @property
+    def description(self):
+        return self.__name
 
-    def getDescription(self):      
-        return self.description
+    @description.setter
+    def description(self, value):
+        self.__description = value
 
-    # Commands   
-    def addCommand(self,command):  
-        self.commands.append(command)
+    # Commands
+    def add_command(self,command):  
+        self.__commands.append(command)
 
     # Execute all commands and compare the result
-    def getResult(self):
+    @property
+    def result(self):
         rt = True
-        res = self.commands[0].exec()
-        for c in self.commands:
+        res = self.__commands[0].exec()
+        for c in self.__commands:
             if res != c.exec():
                 rt = False
                 break
         return rt
 
-class html:
+class HTML:
     instructions = {}
 
     # constructor
     def __init__(self, instructions):  
         self.instructions = instructions
 
-    def getFileContent(self):
-        return self.getTableHead()
+    @property
+    def file_content(self):
+        return self.__generate_table()
 
-    def getHead(self,cols):
+    def __gen_head(self,cols):
         txt = "<tr>"
         for c in cols:
            txt += "<th>{}</th>".format(c)
         txt += "</tr>"
         return txt
 
-    def getLine(self,cols):
+    def __gen_line(self,cols):
         txt = "<tr>"
         for c in cols:
            txt += "<td>{}</td>".format(c)
         txt += "</tr>"
         return txt
 
-    def generateTable(self):
+    def __generate_table(self):
         txt = "<table style=\"width:100%\">"
-        txt += self.getHead(['Name','Description','Result'])
+        txt += self.__gen_head(['Name','Description','Result'])
         for key in self.instructions:
             inst = self.instructions[key]
-            txt += self.getLine([inst.getName(),
-                                inst.getDescription(),
-                                inst.getResult()])
+            txt += self.__gen_line([inst.name,
+                                inst.description,
+                                inst.result])
         txt += "</table>"
         return txt
         
 
-    def generateFile(self):
+    def generate_file(self):
         file_name = 'dashboard.html'
-        txt = self.generateTable()
+        txt = self.file_content
         f = open(file_name,'wb')
         f.write(txt.encode())
         f.close()
@@ -141,17 +138,17 @@ class html:
 def createCommand(node):
     exec = node.find('executable').text
     std = node.attrib['std']
-    rt = command(exec,std)
+    rt = Command(exec,std)
     for a in node.findall('arg'):
-       rt.addArg(a.text)
+       rt.add_arg(a.text)
     return rt
 
 # Instruction factory
 def createInstructions(node):
-    rt = instruction(node.tag)
-    rt.setDescription(node.find('description').text)
+    rt = Instruction(node.tag)
+    rt.description = node.find('description').text
     for c in node.findall('command'):
-        rt.addCommand(createCommand(c))
+        rt.add_command(createCommand(c))
     print(rt)
     return rt
 
@@ -168,9 +165,9 @@ def main():
     inst_dict = parseInstructions()
 
     print("Generate html...")
-    html_file = html(inst_dict)
-    html_file.generateFile()
-    webbrowser.open_new_tab(html_file.generateFile())
+    html_file = HTML(inst_dict)
+    html_file.generate_file()
+    webbrowser.open_new_tab(html_file.generate_file())
     
 if __name__ == "__main__":
     main()
